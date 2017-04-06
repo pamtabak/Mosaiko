b0be1e92d1406373b4bf026da3120bc116db9f53
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2017-04-05 21:58:22 -0300

    only playing audio when not walking

--- Mosaiko/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Mosaiko/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -156,7 +156,10 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             m_NextStep = m_StepCycle + m_StepInterval;
 
-            PlayFootStepAudio();
+            if (!m_IsWalking)
+            {
+                PlayFootStepAudio();
+            }
         }
 
 
